674fc90fc08d5ec6857c2777b260876c43a2f831
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-10 21:28:29 -0400

    delete old code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -25,28 +25,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
-
-
-
-
-
-// import React from 'react';
-// import { useQuery } from '@apollo/client';
-
-
-
-
-
-// const Home = () => {
-
-
-//   return (
-//     <main>
-//         <p>Test</p>
-//     </main>
-//   );
-// };
-
-// export default Home;
