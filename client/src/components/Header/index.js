c9b670b48056c547763624f154bb32056e10201c
Dexter Vorbe <dextervorbe@ymail.com>
2021-08-11 19:16:28 -0400

    Added Links
    Added Home, Create character link to nav and changed user's profile to user's character

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -20,9 +20,11 @@ const Header = () => {
         <div>
           {Auth.loggedIn() ? (
             <>
+            <Link className="btn btn-lg btn-info m-2" to="/"> Home </Link>
               <Link className="btn btn-lg btn-info m-2" to="/me">
-                {Auth.getProfile().data.username}'s profile
+                {Auth.getProfile().data.username}'s Characters
               </Link>
+              <Link className="btn btn-lg btn-info m-2" to="/createChar">Create Character</Link>
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
               </button>
