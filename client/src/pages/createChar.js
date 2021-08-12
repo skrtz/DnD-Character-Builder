9e1bdb3a57dc1dbfdeed3e063f95b723318cb7c0
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-12 19:49:15 -0400

    add character options

--- client/src/pages/createChar.js
+++ client/src/pages/createChar.js
@@ -5,20 +5,29 @@ import { Form } from "react-bootstrap";
 const createChar = () => {
     return (
         <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
+            <Form.Group className="mb-3" controlId="charRace">
+                <Form.Label>Enter Race</Form.Label>
+                <Form.Control type="input" placeholder="Enter Race" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="mb-3" controlId="charClass">
+                <Form.Label>Enter Class</Form.Label>
+                <Form.Control type="password" placeholder="Enter Class" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+
+            <Form.Group className="mb-3" controlId="charAbilities">
+                <Form.Label>Ability Scores</Form.Label>
+                <Form.Control type="input" placeholder="Strength" />
+                <Form.Control type="input" placeholder="Dexterity" />
+                <Form.Control type="input" placeholder="Constitution" />
+                <Form.Control type="input" placeholder="Intelligence" />
+                <Form.Control type="input" placeholder="Wisdom" />
+                <Form.Control type="input" placeholder="Charisma" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="charBackground">
+                <Form.Label>Enter Class</Form.Label>
+                <Form.Control type="password" placeholder="Enter Class" />
             </Form.Group>
             <button variant="primary" type="submit">
                 Submit
