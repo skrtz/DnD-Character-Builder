62cda22c505cf2e9ec781f1ae5c68de92bce3bbc
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-12 13:16:07 -0400

    render CreateChar page

--- client/src/pages/createChar.js
+++ client/src/pages/createChar.js
@@ -20,9 +20,9 @@ const createChar = () => {
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <button variant="primary" type="submit">
                 Submit
-            </Button>
+            </button>
         </Form>
     );
 };
