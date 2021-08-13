6e3c9549a470d62cf4670eb821ce41dd561482fb
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-08-13 14:46:03 -0400

    create char page and form component

--- client/src/components/BuildForm/CharForm.js
+++ client/src/components/BuildForm/CharForm.js
@@ -6,55 +6,155 @@ import { ADD_CHAR } from '../../utils/mutations';
 
 const CharForm = () => {
     const [formState, setFormState] = useState({
-        thoughtText: '',
-        thoughtAuthor: '',
+        name: '',
+        race: '',
+        class: '',
+        background: '',
+        stats: {
+            strength: '',
+            dexterity: '',
+            constitution: '',
+            intelligence: '',
+            wisdom: '',
+            charisma: '',
+        },
+        alignment: '',
+        weapons: ''
     });
 
+    const [addChar, { error }] = useMutation(ADD_CHAR);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        // On form submit, perform mutation and pass in form data object as arguments
+        // It is important that the object fields are match the defined parameters in `ADD_CHAR` mutation
+        try {
+            const { data } = addChar({
+                variables: { ...formState },
+            });
+
+            window.location.reload();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
-        <Form>
+        <Form onSubmit={handleFormSubmit}>
             <Form.Group className="mb-3" controlId="charName">
                 <Form.Label>Enter Name</Form.Label>
-                <Form.Control type="input" placeholder="Enter Name" />
+                <Form.Control
+                    type="input"
+                    placeholder="Enter Name"
+                    value="name"
+                />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="charRace">
                 <Form.Label>Enter Race</Form.Label>
-                <Form.Control type="input" placeholder="Enter Race" />
+                <Form.Control
+                    type="input"
+                    placeholder="Enter Race"
+                    value="race"
+                />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="charClass">
                 <Form.Label>Enter Class</Form.Label>
-                <Form.Control type="password" placeholder="Enter Class" />
+                <Form.Control
+                    type="input"
+                    placeholder="Enter Class"
+                    value="class"
+                />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="charAbilities">
+            <Form.Group className="mb-3" controlId="charAbilities" value="stats">
                 <Form.Label>Ability Scores</Form.Label>
-                <Form.Control type="input" placeholder="Strength" />
-                <Form.Control type="input" placeholder="Dexterity" />
-                <Form.Control type="input" placeholder="Constitution" />
-                <Form.Control type="input" placeholder="Intelligence" />
-                <Form.Control type="input" placeholder="Wisdom" />
-                <Form.Control type="input" placeholder="Charisma" />
+                <Form.Control
+                    type="input"
+                    placeholder="Strength"
+                    value="strength"
+                />
+                <Form.Control
+                    type="input"
+                    placeholder="Dexterity"
+                    value= "dexterity"
+                />
+                <Form.Control 
+                    type="input" 
+                    placeholder="Constitution" 
+                    value="constitution"
+                />
+                <Form.Control 
+                    type="input" 
+                    placeholder="Intelligence" 
+                    value="intelligence"
+                />
+                <Form.Control 
+                    type="input" 
+                    placeholder="Wisdom" 
+                    value="wisdom"
+                />
+                <Form.Control 
+                    type="input" 
+                    placeholder="Charisma" 
+                    value="charisma"
+                />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="charBackground">
                 <Form.Label>Enter Character Background</Form.Label>
-                <Form.Control type="password" placeholder="Enter " />
+                <Form.Control 
+                    type="input" 
+                    placeholder="Enter Background" 
+                    value="background"
+                />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="charAlignment">
                 <Form.Label>Enter Alignment</Form.Label>
-                <Form.Control type="input" placeholder="Enter Alignment" />
+                <Form.Control 
+                    type="input" 
+                    placeholder="Enter Alignment" 
+                    value="alignment"
+                />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="charBackground">
-                <Form.Label>Enter Background</Form.Label>
-                <Form.Control type="input" placeholder="Enter Background" />
+            <Form.Group className="mb-3" controlId="charItems">
+                <Form.Label>Enter Items</Form.Label>
+                <Form.Control 
+                    type="input" 
+                    placeholder="Enter Items" 
+                    value="items"
+                />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="charWeapons">
                 <Form.Label>Enter Weapons</Form.Label>
-                <Form.Control type="input" placeholder="Enter Weapons" />
+                <Form.Control 
+                    type="input" 
+                    placeholder="Enter Weapons" 
+                    value="weapons"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="charHP">
+                <Form.Label>Enter HitPoints</Form.Label>
+                <Form.Control 
+                    type="input" 
+                    placeholder="Enter HP" 
+                    value="hitPoints"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="charLevel">
+                <Form.Label>Enter Level</Form.Label>
+                <Form.Control 
+                    type="input" 
+                    placeholder="Enter Level" 
+                    value="level"
+                />
             </Form.Group>
 
             <button variant="primary" type="submit">
