fca87c5dd1e54a431763c28780ac55dd660f5cd9
Dan Ringenbach <danringenbach.art@gmail.com>
2021-08-15 15:19:58 -0400

    delete character works

--- client/src/components/CharacterList/Characters.js
+++ client/src/components/CharacterList/Characters.js
@@ -1,32 +1,54 @@
-import React from 'react';
-import { Card, ListGroupItem, ListGroup, Col, Row, Button } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
-import { DELETE_CHAR } from '../../utils/mutations';
-
-
+import React from "react";
+import {
+  Card,
+  ListGroupItem,
+  ListGroup,
+  Col,
+  Row,
+  Button,
+} from "react-bootstrap";
+import { useMutation, useQuery } from "@apollo/client";
+import { DELETE_CHAR } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+import { QUERY_ME } from "../../utils/queries";
 
 const Character = ({ characters }) => {
-  
+  const [removeChar] = useMutation(DELETE_CHAR);
+  const { loading, data } = useQuery(QUERY_ME);
 
-  
+  const userData = data
+  console.log(userData)
+
+  const handleRemoveChar = async (characterId) => {
+    
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      await removeChar({
+        variables: { characterId },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   if (!characters.length) {
     return <h3>No characters yet</h3>;
-
   }
-  console.log()
+  console.log();
   return (
     <div id="characterCards">
       <Row>
         {characters &&
           characters.map((character) => (
-            <Col style={{marginBottom: "20px"}}>
-              <Card style={{ width: '200px' }} >
+            <Col style={{ marginBottom: "20px" }}>
+              <Card key={character._id} style={{ width: "200px" }}>
                 <Card.Img variant="top" src={character.image} />
                 <Card.Body>
                   <Card.Title>{character.name}</Card.Title>
-                  <Card.Text>
-                    {character.background}
-                  </Card.Text>
+                  <Card.Text>{character.background}</Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
                   <ListGroupItem>Race: {character.race}</ListGroupItem>
@@ -35,7 +57,12 @@ const Character = ({ characters }) => {
                 </ListGroup>
                 <Card.Body>
                   <Button href="#">Edit</Button>
-                  <Button href="#">Delete</Button>
+                  <Button
+                    href="#"
+                    onClick={() => handleRemoveChar(character._id)}
+                  >
+                    Delete
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
@@ -44,6 +71,5 @@ const Character = ({ characters }) => {
     </div>
   );
 };
-
 
 export default Character;
--- client/src/pages/createChar.js
+++ client/src/pages/createChar.js
@@ -8,7 +8,7 @@ const CreateChar = () => {
     return (
         <>
         <CharForm />
-        <DiceComponent />
+        {/* <DiceComponent /> */}
         </>
     )
 };
